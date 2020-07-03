ed5f6b21fbec9c460f927e4947df00e94596e1fd
Krikiboli <mulderf0f@gmail.com>
2020-07-03 15:36:01 +1000

    atkin_0.5

--- atkin.py
+++ atkin.py
@@ -8,108 +8,85 @@ from colorama import init
 
 init(autoreset=True)
 
-
 def atkin(limit: int, proc: int) -> None:
     print(f"•Выполняется 'Решето Аткина' {proc} ")
-    count_list = [False for i in range(limit + 1)]
-    sqrt_of_limit = sqrt(limit)
-    for i in range(proc, int(sqrt_of_limit + 1), 3):
-        for j in range(1, int(sqrt_of_limit + 1)):
-            x = i * i
-            y = j * j
-            n = 4 * x + y
-            if n <= limit and (n % 12 == 1 or n % 12 == 5):
-                count_list[n] = True
-            if n <= limit and n % 13 == 0:
-                count_list[n] = False
-            n -= x
-            if n <= limit and n % 12 == 7:
-                count_list[n] = True
-            if n <= limit and n % 13 == 0:
-                count_list[n] = False
-            n -= 2 * y
-
-            if i > j and n <= limit and n % 12 == 11:
-                count_list[n] = True
-            if n <= limit and n % 13 == 0:
-                count_list[n] = False
-    for i in range(5, int(sqrt_of_limit + 1)):
-        if count_list[i]:
-            number = i * i
-            for j in range(number, int(sqrt_of_limit + 1), number):
-                count_list[j] = False
-
+    count_list = [False] * (limit + 1)
+    for x in range(proc, int(sqrt(limit)) + 1, 3):
+        for y in range(1, int(sqrt(limit)) + 1):
+            n = 4 * x ** 2 + y ** 2
+            if n <= limit and n % 5 != 0 and (n % 12 == 1 or n % 12 == 5):
+                count_list[n] = not count_list[n]
+            n = 3 * x ** 2 + y ** 2
+            if n <= limit and n % 5 != 0 and n % 12 == 7:
+                count_list[n] = not count_list[n]
+            n = 3 * x ** 2 - y ** 2
+            if x > y and n <= limit and n % 5 != 0 and n % 12 == 11:
+                count_list[n] = not count_list[n]
     if proc == 1:
-        with open('one.txt', "w") as file:
+        with open('one.txt', "w", encoding='utf-7') as file:
             for x in count_list:
                 string = str(x) + '\n'
                 file.write(string)
     elif proc == 2:
-        with open('two.txt', "w") as file:
+        with open('two.txt', "w", encoding='utf-7') as file:
             for x in count_list:
                 string = str(x) + '\n'
                 file.write(string)
     else:
-        with open('three.txt', "w") as file:
+        with open('three.txt', "w", encoding='utf-7') as file:
             for x in count_list:
                 string = str(x) + '\n'
                 file.write(string)
 
 
 def write_in_file() -> None:
-    with open('one.txt', 'r') as one:
+    with open('one.txt', 'r', encoding='utf-8') as one:
         first_read = one.read()
         list_1 = first_read.split("\n")
-    with open('two.txt', 'r') as two:
+    with open('two.txt', 'r', encoding='utf-8') as two:
         first_read = two.read()
         list_2 = first_read.split("\n")
-    with open('three.txt', 'r') as three:
+    with open('three.txt', 'r', encoding='utf-8') as three:
         first_read = three.read()
         list_3 = first_read.split("\n")
 
     os.remove('one.txt')
     os.remove('two.txt')
     os.remove('three.txt')
-
-    dirty_list = [0] * len(list_1)
+    dirty_list = [False] * len(list_1)
     for i in range(0, len(list_1)):
         if list_1[i] == "False":
-            list_1_elem = 0
+            a = False
         else:
-            list_1_elem = 1
+            a = True
         if list_2[i] == "False":
-            list_2_elem = 0
+            b = False
         else:
-            list_2_elem = 1
+            b = True
         if list_3[i] == "False":
-            list_3_elem = 0
+            c = False
         else:
-            list_3_elem = 1
-        dirty_list[i] = (list_1_elem + list_2_elem + list_3_elem) % 2
-    nums = [0] * len(dirty_list)
-    for i in range(len(dirty_list)):
-        if dirty_list[i] == 1:
-            if i % 5 != 0:
-                nums[i] = i
+            c = True
+        dirty_list[i] = (a + b + c) % 2
+    nums = [False] * len(dirty_list)
+    for index, x in enumerate(dirty_list):
+        if x == 1:
+            nums[index] = index
+    for x in range(5, int(sqrt(len(dirty_list)))):
+        if nums[x]:
+            for y in range(x ** 2, limit + 1, x ** 2):
+                nums[y] = False
 
-    nums[2] = 2
-    nums[3] = 3
-    nums[5] = 5
-    nums[13] = 13
-
-    for i in range(2, len(nums)):
-        if nums[i] != 0:
-            for j in range(i * i, limit + 1, i * i):
-                nums[j] = 0
-    print(Fore.GREEN + '•Запись результата в файл')
     nums.pop()
-    with open('result.txt', 'w') as file:
-        file.write('')
-        result = ''
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                result += f'{nums[i]}\n'
-        file.write(result)
+    result = list()
+    for index, elem in enumerate(nums):
+        if elem is not False:
+            result.append(elem)
+    with open("result.txt", "w", encoding='utf-8') as file:
+        file.write("2\n3\n5\n")
+        for i in result:
+            str0 = '' + str(i) + "\n"
+            file.write(str0)
 
 
 def start(limit: int) -> None:
@@ -131,5 +108,5 @@ if __name__ == '__main__':
         start_time = timeit.default_timer()
         start(limit)
         write_in_file()
-        print(f'•Вычисление длилось: {timeit.default_timer() - start_time} секунд\n')
-        print(Fore.GREEN + 'Скрипт завершен\nРезультат в папке с относительным путем')
+        print(Fore.GREEN + f'•Вычисление длилось: {timeit.default_timer() - start_time} секунд\n')
+        print(Fore.GREEN + '•Скрипт завершен\n•Результат в папке с относительным путем')
